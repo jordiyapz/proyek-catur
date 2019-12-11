e73a8b91610b370dc0a2b54744e1aafbbfe07a22
Jordi Yaputra <Jordiyaputra@gmail.com>
2019-12-12 02:33:55 +0700

    added castling

--- classes/chessmen/pieces/King.js
+++ classes/chessmen/pieces/King.js
@@ -2,6 +2,7 @@ class King extends Piece {
     constructor(x, y, isWhite) {
         super(x, y, isWhite, 5, 'king');
         this.castlingable = true;
+        this.castableRooks = [];
     }
 
     clone() {
@@ -10,22 +11,29 @@ class King extends Piece {
     }
 
     move(x, y) {
+        if (this.castlingable) {
+            const c = this.coord;
+            if (Math.abs(x - c.x) == 2) {
+                const vec = createVector(x, y);
+                const fac = (x < c.x)? -1:1;
+                for (const rook of this.castableRooks) {
+                    console.log(rook);
+                    if (x*fac < rook.coord.x*fac)
+                        rook.move(x-fac, y);
+                    rook.castlingable = false;
+                }
+            }
+        }
         this.coord.set(x, y);
         this.castlingable = false;
+        this.castableRooks = [];
     }
 
     getHashMoves(pieces) {
         const c = this.coord;
         const moves = [];
 
-        let friends, foes;
-        if (this.isWhite) {
-            friends = pieces.white;
-            foes = pieces.black;
-        } else {
-            friends = pieces.black;
-            foes = pieces.white;
-        }
+        const {friends} = this.getFriendsFoes(pieces);
 
         for (let vec = createVector(c.x-1, c.y-1);
             vec.y <= c.y+1; vec.add(0,1)
@@ -44,6 +52,36 @@ class King extends Piece {
                 if (!skip) moves.push(vec.copy());
             }
         }
+        if (!this.isOnCheck && this.castlingable)
+            this._modCastlingMoves(moves, friends);
         return moves;
+    }
+
+    _modCastlingMoves (moves, friends) {
+        const c = this.coord;
+        const castableRooks = [];
+        this.castableRooks = [];
+        for (const p of friends) {
+            if (p.type == 'rook' && p.castlingable) castableRooks.push(p);
+            if (castableRooks.length >= 2) break;
+        }
+        for (const rook of castableRooks) {
+            const rx = rook.coord.x;
+            const fac = (rx < c.x)? -1:1;
+            const vec = createVector(c.x+fac, c.y);
+            let stop = false;
+            for (; !stop && Math.abs(vec.x - rx) > 0; vec.x+=fac) {
+                for (const p of friends) {
+                    if (p.coord.equals(vec)) {
+                        stop = true;
+                        break;
+                    }
+                }
+            }
+            if (!stop) {
+                moves.push(createVector(c.x+2*fac, c.y));
+                this.castableRooks.push(rook);
+            }
+        }
     }
 }
