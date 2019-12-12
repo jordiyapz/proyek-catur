77044755007a90749968fcfcf8050e53478b6c5b
Jordi Yaputra <Jordiyaputra@gmail.com>
2019-12-12 21:00:08 +0700

    Update Button.js

--- classes/buttons/Button.js
+++ classes/buttons/Button.js
@@ -4,36 +4,33 @@ class Button {
         this.pos = createVector(x, y);
         if (height === undefined) height = width;
         this.dim = {width, height};
-    }
-    setOnClick (onClick) {
-        this.onClick = onClick;
-        return this;
-    }
-    setOnHover (onHover) {
-        this.onHover = onHover;
-        return this;
+        this.onClick = null;
+        this.onMouseOver = null;
+        this.onMouseOut = null;
     }
     setImage (img) {
         this.img = img;
         return this;
     }
     listenClick() {
-        if (this.isOnClick())
+        if (this.isClicked())
             this.onClick();
     }
     listenHover() {
-        if (this.isOnHover())
-            this.onHover();
+        if (this.isHovered() && this.onMouseOver)
+            this.onMouseOver();
+        else if (this.onMouseOut)
+            this.onMouseOut();
     }
-    isOnHover() {
+    isHovered() {
         const mouse = createVector(mouseX, mouseY);
         const {pos, dim} = this;
         if (mouse.x >= pos.x && mouse.x <= pos.x+dim.width &&
             mouse.y >= pos.y && mouse.y <= pos.y+dim.height) return true;
         return false;
     }
-    isOnClick() {
-        return (this.isOnHover() && mouseButton == LEFT);
+    isClicked() {
+        return (this.isHovered() && mouseButton == LEFT);
     }
     render () {
         const {pos, dim} = this;
