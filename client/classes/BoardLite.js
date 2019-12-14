85150667cc67baedb376311904584b37546830b1
Jordi Yaputra <Jordiyaputra@gmail.com>
2019-12-14 22:20:12 +0700

    tidying up some more

--- client/classes/BoardLite.js
+++ client/classes/BoardLite.js
@@ -2,8 +2,10 @@ class BoardLite {
     constructor(pieces) {
         this.turn = 1; // 0 == black, 1 == white;
         this.isOnCheck = false;
-        this.pieces = (pieces)? pieces:{white:[], black:[]};
-        this.setupPieces(pieces);
+        if (!pieces) {
+            this.pieces = {white:[], black:[]};
+            this.setupPieces();
+        } else this.pieces = pieces;
         this.cache = {};
     }
 
@@ -11,33 +13,32 @@ class BoardLite {
      * Summary heart of Board constructor helper :) .
      * @param {Object} pieces template consisting of white and black pieces
      */
-    setupPieces(pieces) {
-        if (!pieces) {
-            for (let i = 0; i < 8; i++) {
-                this.pieces.black.push(new Pawn(i, 1, false));
-                this.pieces.white.push(new Pawn(i, 6, true));
-            }
-            this.pieces.black.push(
-                new Rook    (0, 0, false),
-                new Knight  (1, 0, false),
-                new Bishop  (2, 0, false),
-                new Queen   (3, 0, false),
-                new King    (4, 0, false),
-                new Bishop  (5, 0, false),
-                new Knight  (6, 0, false),
-                new Rook    (7, 0, false)
-            );
-            this.pieces.white.push(
-                new Rook    (0, 7, true),
-                new Knight  (1, 7, true),
-                new Bishop  (2, 7, true),
-                new Queen   (3, 7, true),
-                new King    (4, 7, true),
-                new Bishop  (5, 7, true),
-                new Knight  (6, 7, true),
-                new Rook    (7, 7, true)
-            );
+    setupPieces() {
+        for (let i = 0; i < 8; i++) {
+            this.pieces.black.push(new Pawn(i, 1, false));
+            this.pieces.white.push(new Pawn(i, 6, true));
         }
+        this.pieces.black.push(
+            new Rook    (0, 0, false),
+            new Knight  (1, 0, false),
+            new Bishop  (2, 0, false),
+            new Queen   (3, 0, false),
+            new King    (4, 0, false),
+            new Bishop  (5, 0, false),
+            new Knight  (6, 0, false),
+            new Rook    (7, 0, false)
+        );
+        this.pieces.white.push(
+            new Rook    (0, 7, true),
+            new Knight  (1, 7, true),
+            new Bishop  (2, 7, true),
+            new Queen   (3, 7, true),
+            new King    (4, 7, true),
+            new Bishop  (5, 7, true),
+            new Knight  (6, 7, true),
+            new Rook    (7, 7, true)
+        );
+
     }
 
     clone() {
